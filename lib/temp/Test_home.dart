a155e32fc4be1c0795bf2f0d8347ed4bcb55b6fe
Poleksey <poleksey.internationalization@gmail.com>
2024-12-13 03:37:00 +0300

    завершен подготовительный этап

--- lib/temp/Test_home.dart
+++ lib/temp/Test_home.dart
@@ -1,18 +1,8 @@
-import 'dart:convert';
-
-import 'package:app_2/elements/ProductCard.dart';
-
-// import 'package:app_2/temp/test%20copy.dart';
-// import 'package:app_2/pages/product.dart';
 import 'package:app_2/temp/test_product_page.dart';
-import 'package:dio/dio.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:app_2/temp/test.dart';
-// import 'package:app_2/temp/test copy.dart';
-import 'package:app_2/temp/test_product_page.dart';
+
 import 'package:app_2/temp/test_large_items.dart';
-import 'Popup_menu.dart';
 
 class TestHome_1 extends StatefulWidget {
   // const TestHome_1({super.key});
@@ -23,20 +13,6 @@ class TestHome_1 extends StatefulWidget {
 
 class TestHome extends State<TestHome_1> {
   List<Item>? items;
-  List<Item> items_1 = getItemsList_3();
-  // void getItemsList_1() async {
-  //   final response = await Dio().get(
-  //     'https://github.com/Poleksey/localization_f/blob/main/lib/data/list_of_products_large.json',
-  //   );
-  //   final jsonData = jsonDecode(response.data) as Map<String, dynamic>;
-  //   final itemData = (jsonData['items'] as List<dynamic>)
-  //       .map((item) => Item.fromJson(item as Map<String, dynamic>))
-  //       .toList();
-
-  //   setState(() {
-  //     items = itemData;
-  //   });
-  // }
 
   void initState() async {
     super.initState();
@@ -48,10 +24,6 @@ class TestHome extends State<TestHome_1> {
     items = await getItemsList_1();
     setState(() {});
   }
-
-  // const TestHome({super.key, });
-
-  // final column_of_cards = ProductCardsRepository().loadItemsFromJson();
 
   @override
   Widget build(BuildContext context) {
@@ -84,7 +56,6 @@ class TestHome extends State<TestHome_1> {
                               child: Image.network(superitem.imageUrl),
                             ),
                           ),
-
                           Row(children: [
                             Container(
                                 child: Text(
@@ -115,21 +86,11 @@ class TestHome extends State<TestHome_1> {
                               ),
                             ),
                           ),
-
-                          // SizedBox(
-                          //     child: Container(
-                          //         margin: EdgeInsets.only(top: 20),
-                          //         child: ListTile(
-                          //           // leading: Image.network(item.imageUrl),
-                          //           title: Text(item.title),
-                          //           subtitle: Text('${item.price} ${item.currency}'),
-                          //         )))
                         ]),
                   ),
                   Opacity(
                     opacity: 0,
                     child: SizedBox(
-                      // width: double.infinity,
                       height: double.infinity,
                       width: double.infinity,
                       child: ElevatedButton(
@@ -160,26 +121,3 @@ class TestHome extends State<TestHome_1> {
     );
   }
 }
-
-// class TestHome extends StatelessWidget {
-//   TestHome({super.key});
-//   // final column_of_cards = ProductCardsRepository().loadItemsFromJson();
-//   final len = items.length;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: ListView.builder(
-//       itemCount: len,
-//       itemBuilder: (context, index) {
-//         final item = items[index];
-//         return Container(
-
-//             child: ListTile(
-//           leading: Image.network(item.imageUrl),
-//           title: Text(item.title),
-//           subtitle: Text('${item.price} ${item.currency}'),
-//         ));
-//       },
-//     ));
-//   }
-// }
--- lib/temp/test_large_items.dart
+++ lib/temp/test_large_items.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
 
 class Item {
   final String id;
@@ -32,69 +30,22 @@ class Item {
   }
 }
 
-String jsonString = '''
-   {
-  "items": [
- 
-    {
-      "id": "1",
-      "title": "Металлический кубик",
-      "price": "1000",
-      "currency": "₽",
-      "description": "Металлический куб - это трехмерный геометрический объект, каждая из шести граней которого представляет собой идеально ровный квадратный лист металла. Все ребра куба равны, их длина одинакова, что делает форму максимально симметричной. Поверхность куба имеет гладкую, зеркальную текстуру, которая отражает окружающие предметы. Металл, из которого изготовлен куб, может варьироваться - это может быть сталь, медь, алюминий или другой прочный, плотный металлический сплав.Сочетание строгих геометрических линий и гладкой блестящей поверхности придает металлическому кубу солидный, монолитный вид. Он выглядит надежным, долговечным и обладает ощущением массивности, несмотря на свои компактные размеры",
-      "imageUrl": "https://atlas-content-cdn.pixelsquid.com/stock-images/silver-cube-3AaJe6D-600.jpg"
-    },
-    {
-      "id": "2",
-      "title": "Breakfast Earlgrey",
-      "price": "200",
-      "currency": "₽",
-      "description": "Earl Grey Breakfast - это изысканный и многогранный черный чай, который сочетает в себе классические ноты традиционного Earl Grey и более насыщенные, крепкие оттенки завтрачного чая.",
-      "imageUrl": "https://napitkistore.ru/wp-content/uploads/2017/07/twg-breakfast-earl-grey-tea-15.webp"
-    },
-    {
-      "id": "3",
-      "title": "Лампочка",
-      "price": "289",
-      "currency": "₽",
-      "description": "Энергосберегающая светодиодная лампа нового поколения обеспечивает равномерное и мягкое освещение, подходящее для любого помещения. Благодаря высокому качеству и долговечности, лампа прослужит вам долгие годы, при этом значительно экономя электроэнергию по сравнению с традиционными лампами накаливания. Простота установки и безопасность в использовании делают эту лампу идеальным решением для вашего дома или офиса.",
-      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAaaRtuwCq0Q7TT9Kflz7-bhHDDIbZ3BZfrA&s"
-    },
-    {
-      "id": "4",
-      "title": "Валерианы настойка",
-      "price": "90",
-      "currency": "₽",
-      "description": "Валерианка - традиционное натуральное средство, которое веками помогает справиться со стрессом и тревогой. Данный продукт содержит высококачественный экстракт корня валерианы, который мягко и безопасно оказывает успокаивающее действие на нервную систему, улучшает сон и снимает мышечное напряжение. Не является лекарством. Перед употреблением проконсультируйтесь с врачом.",
-      "imageUrl": "https://images.apteka.ru/original_d18e7d7d-b5ff-4397-a01f-bd66465b0cc1.jpeg"
-    },
-    {
-      "id": "5",
-      "title": "Кофе растворимый NESCAFÉ",
-      "price": "730",
-      "currency": "₽",
-      "description": "Вкусный 100% натуральный растворимый кофе с нежной пенкой",
-      "imageUrl": "https://ir.ozone.ru/s3/multimedia-1-u/wc1000/7216994610.jpg"
-    },
-    {
-      "id": "6",
-      "title": "Сунь Цзы Искусство войны ",
-      "price": "1200",
-      "currency": "₽",
-      "description": "Бессмертная классика. Данное издание содержит полный перевод трактата Сунь-Цзы с подробными комментариями. ",
-      "imageUrl": "https://www.combook.ru/pictures/4/9/0_11866179.jpg"
-    },
-    {
-      "id": "7",
-      "title": "Армированный скотч",
-      "price": "400",
-      "currency": "₽",
-      "description": "Благодаря своим характеристикам, армированный скотч является незаменимым помощником в решении множества задач, где требуется надежное, прочное и универсальное клеящее средство.",
-      "imageUrl": "https://pack-land.ru/components/com_jshopping/files/img_products/full_armirovannyj-skotch.jpg"
-    }
-  ]
+Future<List<Item>> getItemsList_1() async {
+  final response = await Dio().get(
+    'https://raw.githubusercontent.com/Poleksey/localization_f/refs/heads/main/lib/data/list_of_products_large.json',
+  );
+
+  final jsonData = jsonDecode(response.data) as Map<String, dynamic>;
+
+  return (jsonData['items'] as List<dynamic>)
+      .map((itemData) => Item.fromJson(itemData as Map<String, dynamic>))
+      .toList();
 }
-   ''';
+
+
+
+
+
 
 // Future<Map<String, dynamic>> fetchJsonData() async {
 //   final response = await http.get(Uri.parse(
@@ -125,34 +76,78 @@ String jsonString = '''
 //     .map((itemData) => Item.fromJson(itemData as Map<String, dynamic>))
 //     .toList();
 
-Future<List<Item>> getItemsList_1() async {
-  final response = await Dio().get(
-    'https://raw.githubusercontent.com/Poleksey/localization_f/refs/heads/main/lib/data/list_of_products_large.json',
-  );
-  print(response);
-  final jsonData = jsonDecode(response.data) as Map<String, dynamic>;
 
-  // print(jsonData);
-  return (jsonData['items'] as List<dynamic>)
-      .map((itemData) => Item.fromJson(itemData as Map<String, dynamic>))
-      .toList();
-}
 
-// Future<List<Item>> getItemsList_2() async {
-//   final file = File(
-//       'C:/Users/Redmi/Desktop/localization/app_2/assets/json_files/list_of_products_large.json');
-//   final contents = await file.readAsString();
+// String jsonString = '''
+//    {
+//   "items": [
+ 
+//     {
+//       "id": "1",
+//       "title": "Металлический кубик",
+//       "price": "1000",
+//       "currency": "₽",
+//       "description": "Металлический куб - это трехмерный геометрический объект, каждая из шести граней которого представляет собой идеально ровный квадратный лист металла. Все ребра куба равны, их длина одинакова, что делает форму максимально симметричной. Поверхность куба имеет гладкую, зеркальную текстуру, которая отражает окружающие предметы. Металл, из которого изготовлен куб, может варьироваться - это может быть сталь, медь, алюминий или другой прочный, плотный металлический сплав.Сочетание строгих геометрических линий и гладкой блестящей поверхности придает металлическому кубу солидный, монолитный вид. Он выглядит надежным, долговечным и обладает ощущением массивности, несмотря на свои компактные размеры",
+//       "imageUrl": "https://atlas-content-cdn.pixelsquid.com/stock-images/silver-cube-3AaJe6D-600.jpg"
+//     },
+//     {
+//       "id": "2",
+//       "title": "Breakfast Earlgrey",
+//       "price": "200",
+//       "currency": "₽",
+//       "description": "Earl Grey Breakfast - это изысканный и многогранный черный чай, который сочетает в себе классические ноты традиционного Earl Grey и более насыщенные, крепкие оттенки завтрачного чая.",
+//       "imageUrl": "https://napitkistore.ru/wp-content/uploads/2017/07/twg-breakfast-earl-grey-tea-15.webp"
+//     },
+//     {
+//       "id": "3",
+//       "title": "Лампочка",
+//       "price": "289",
+//       "currency": "₽",
+//       "description": "Энергосберегающая светодиодная лампа нового поколения обеспечивает равномерное и мягкое освещение, подходящее для любого помещения. Благодаря высокому качеству и долговечности, лампа прослужит вам долгие годы, при этом значительно экономя электроэнергию по сравнению с традиционными лампами накаливания. Простота установки и безопасность в использовании делают эту лампу идеальным решением для вашего дома или офиса.",
+//       "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAaaRtuwCq0Q7TT9Kflz7-bhHDDIbZ3BZfrA&s"
+//     },
+//     {
+//       "id": "4",
+//       "title": "Валерианы настойка",
+//       "price": "90",
+//       "currency": "₽",
+//       "description": "Валерианка - традиционное натуральное средство, которое веками помогает справиться со стрессом и тревогой. Данный продукт содержит высококачественный экстракт корня валерианы, который мягко и безопасно оказывает успокаивающее действие на нервную систему, улучшает сон и снимает мышечное напряжение. Не является лекарством. Перед употреблением проконсультируйтесь с врачом.",
+//       "imageUrl": "https://images.apteka.ru/original_d18e7d7d-b5ff-4397-a01f-bd66465b0cc1.jpeg"
+//     },
+//     {
+//       "id": "5",
+//       "title": "Кофе растворимый NESCAFÉ",
+//       "price": "730",
+//       "currency": "₽",
+//       "description": "Вкусный 100% натуральный растворимый кофе с нежной пенкой",
+//       "imageUrl": "https://ir.ozone.ru/s3/multimedia-1-u/wc1000/7216994610.jpg"
+//     },
+//     {
+//       "id": "6",
+//       "title": "Сунь Цзы Искусство войны ",
+//       "price": "1200",
+//       "currency": "₽",
+//       "description": "Бессмертная классика. Данное издание содержит полный перевод трактата Сунь-Цзы с подробными комментариями. ",
+//       "imageUrl": "https://www.combook.ru/pictures/4/9/0_11866179.jpg"
+//     },
+//     {
+//       "id": "7",
+//       "title": "Армированный скотч",
+//       "price": "400",
+//       "currency": "₽",
+//       "description": "Благодаря своим характеристикам, армированный скотч является незаменимым помощником в решении множества задач, где требуется надежное, прочное и универсальное клеящее средство.",
+//       "imageUrl": "https://pack-land.ru/components/com_jshopping/files/img_products/full_armirovannyj-skotch.jpg"
+//     }
+//   ]
+// }
+//    ''';
 
-//   final jsonData = jsonDecode(contents) as Map<String, dynamic>;
+
+// final jsonData = jsonDecode(jsonString) as Map<String, dynamic>;
+
+// List<Item> getItemsList_3() {
 //   return (jsonData['items'] as List<dynamic>)
 //       .map((itemData) => Item.fromJson(itemData as Map<String, dynamic>))
 //       .toList();
 // }
 
-final jsonData = jsonDecode(jsonString) as Map<String, dynamic>;
-
-List<Item> getItemsList_3() {
-  return (jsonData['items'] as List<dynamic>)
-      .map((itemData) => Item.fromJson(itemData as Map<String, dynamic>))
-      .toList();
-}
